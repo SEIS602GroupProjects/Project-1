4c4c7a0e5278f727ccabc57cd6544943d101d2b5
mcrawffles <khanezhyray@gmail.com>
2016-03-05 14:54:35 -0600

    Minor changes to adjust for add of Item Class

--- src/PointOfSale/InventorySystem.java
+++ src/PointOfSale/InventorySystem.java
@@ -3,8 +3,11 @@ package PointOfSale;
 public class InventorySystem {
 
 	private IOSystem IO = new IOSystem();
+	private int[] UPCs;
 	private String[] items;
-	private String[] quantities;
+	private int[] quantities;
+	private double[] prices;
+	
 	
 	public InventorySystem()
 	{
@@ -29,18 +32,23 @@ public class InventorySystem {
 		String[] tmp = IO.ReadFile("Data/Inventory.txt");
 
 		// Create the items array and set the size to be the 
+		UPCs = new int[tmp.length];
 		// same as the number of lines in the file
 		items = new String[tmp.length];
 		// Do the same for quantities
-		quantities = new String[tmp.length];
+		quantities = new int[tmp.length];
+		prices = new double[tmp.length];
+		
 		
 		// For each line in the file
 		for (int i=0; i < tmp.length; i++)
 		{
 			// Split the line of the file up at the = sign 			
-			String[] tmp_split = tmp[i].split("=");
-			items[i] = tmp_split[0];
-			quantities[i] = tmp_split[1];
+			String[] tmp_split = tmp[i].split(";");
+			UPCs[i] = Integer.parseInt(tmp_split[0]);
+			items[i] = tmp_split[1];
+			quantities[i] = Integer.parseInt(tmp_split[2]);
+			prices[i] = Double.parseDouble(tmp_split[3]);
 			
 			// For example, tmp[0] == "apples=10" gets stored as
 			// items[0] == "apples"
